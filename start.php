c4c5325b0a239657abee73ee6028d97f2ba1499c
Paweł Sroka <srokap@gmail.com>
2013-12-16 03:32:17 +0100

    Version bump to 1.0.2 and fixed 1.8 compatibility.

--- start.php
+++ start.php
@@ -1,3 +1,25 @@
 <?php
+/**
+ * Elgg 1.8 compatibility
+ */
+if (!function_exists('elgg_get_version')) {
+	function elgg_get_version($human_readable = false) {
+		global $CONFIG;
+
+		static $version, $release;
+
+		if (isset($CONFIG->path)) {
+			if (!isset($version) || !isset($release)) {
+				if (!include($CONFIG->path . "version.php")) {
+					return false;
+				}
+			}
+			return $human_readable ? $release : $version;
+		}
+
+		return false;
+	}
+}
+
 code_review::boot();
 elgg_register_event_handler('init', 'system', array('code_review', 'init'));
