aea5adebaa78b6758c0cd87e676ff4f4eaacfe58
Paweł Sroka <srokap@gmail.com>
2015-01-18 19:46:51 +0100

    Added CodeFixerTest

---
+++ tests/phpunit/CodeFixerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+class CodeFixerTest extends PHPUnit_Framework_TestCase {
+
+	public function testGetBasicFunctionRenames() {
+		$fixes = new CodeFixer();
+
+		$renames = $fixes->getBasicFunctionRenames();
+
+		$pattern = '/^[a-zA-Z_][\sa-zA-Z_0-9\(\)\->]*$/';
+
+		foreach ($renames as $from => $to) {
+			$this->assertNotEmpty($from);
+			$this->assertNotEmpty($to);
+
+			$this->assertRegExp($pattern, $from);
+			$this->assertRegExp($pattern, $to);
+		}
+
+		/*
+		 * Check version filtering
+		 */
+		// no results below 1.7
+		$this->assertEmpty($fixes->getBasicFunctionRenames('1.6'));
+
+		// version 1.7
+		$renames = $fixes->getBasicFunctionRenames('1.7');
+		$this->assertArrayHasKey('elgg_validate_action_url', $renames);
+		$this->assertArrayNotHasKey('register_elgg_event_handler', $renames);
+		$this->assertArrayNotHasKey('setup_db_connections', $renames);
+
+		// version 1.8
+		$renames = $fixes->getBasicFunctionRenames('1.8');
+		$this->assertArrayHasKey('elgg_validate_action_url', $renames);
+		$this->assertArrayHasKey('register_elgg_event_handler', $renames);
+		$this->assertArrayNotHasKey('setup_db_connections', $renames);
+
+		// version 1.9
+		$renames = $fixes->getBasicFunctionRenames('1.9');
+		$this->assertArrayHasKey('elgg_validate_action_url', $renames);
+		$this->assertArrayHasKey('register_elgg_event_handler', $renames);
+		$this->assertArrayHasKey('setup_db_connections', $renames);
+
+		// all versions
+		$renames = $fixes->getBasicFunctionRenames('');
+		$this->assertArrayHasKey('elgg_validate_action_url', $renames);
+		$this->assertArrayHasKey('register_elgg_event_handler', $renames);
+		$this->assertArrayHasKey('setup_db_connections', $renames);
+	}
+}
