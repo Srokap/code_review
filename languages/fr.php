a033e7daf27c952e527233435cf71e2e56e749d1
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-05-18 13:57:11 +0200

    French translation

---
+++ languages/fr.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file was created by Translation Editor v4.0
+ * On 2016-05-18 13:54
+ @author Translated by Florian DANIEL aka Facyla
+ */
+
+return array (
+  'admin:code' => 'Code',
+  'admin:code:diagnostic' => 'Analyse du code',
+  'admin:code:diagnostic:deprecated_list' => 'Liste des fonctions dépréciées',
+  'admin:code:diagnostic:functions_list' => 'Fonctions définies',
+  'admin:code:diagnostic:private_list' => 'Liste des fonctions privées',
+  'code_review:menu' => 'Menu',
+  'code_review:deprecated_list:title' => 'Déprécié depuis la version %s',
+  'code_review:deprecated_list:name' => 'Nom',
+  'code_review:deprecated_list:remarks' => 'Remarques',
+  'code_review:deprecated_list:solution' => 'Solution',
+  'code_review:deprecated_list:file_line' => 'Dans le fichier %s ligne %d',
+  'code_review:private_list:title' => 'Fonctions privées',
+  'code_review:private_list:name' => 'Nom',
+  'code_review:private_list:file_line' => 'Dans le fichier %s ligne %d',
+  'code_review:private_list:reason' => 'Raison pour laquelle la fonction est privée',
+  'code_review:private_list:reason:private' => 'Marquer comme privé',
+  'code_review:private_list:reason:not_documented' => 'Non documenté',
+  'code_review:functions_list:name' => 'Nom',
+  'code_review:functions_list:file' => 'Fichier de définition',
+  'code_review:functions_list:line' => 'Ligne',
+  'code_review:functions_list:title' => 'Fonctions définies dans %s',
+  'code_review:solution:basic_replace_with' => 'Remplacement simple avec %s',
+  'code_review:form' => 'Options',
+  'code_review:results' => 'Résultats',
+  'code_review:results:initial_stub' => 'Sélectionnez les options et envoyez le formulaire pour effectuer l\'analyse. Cette opération peut prendre un temps significatif - veuillez patienter.',
+  'code_review:error:request' => 'Une erreur est survenue lors de l\'envoi de la requête',
+  'code_review:subpath' => 'Sous-dossier à analyser',
+  'code_review:subpath:placeholder' => '(racine de l\'installation)',
+  'code_review:version' => 'Version maximum à analyser',
+  'code_review:disabled_plugins_only' => 'Inclure les plugins désactivés',
+  'code_review:find_deprecated_functions' => 'Recherche l\'utilisation de fonctions dépréciées',
+  'code_review:find_private_functions' => 'Recherche l\'utilisation de fonctions privées',
+  'code_review:fix_problems' => 'Essayer de résoudre les problèmes',
+  'code_review:fix_problems:warning:header' => 'Attention ! Lisez ce qui suit attentivement',
+  'code_review:js:confirm_changes' => 'Confirmez-vous vouloir modifier le code source ? Il s\'agit de votre dernière chance d\'annuler l\'opération !',
+  'code_review:fix_problems:warning' => 'L\'analyseur de code va essayer de résoudre les problème rencontrés. Les modifications seront irréversibles et ne garantissent pas la compatibilité avec les versions précédentes.
+Veuillez vous assurer que vous disposez d\'une archive du code. L\'utilisation de cette fonctionnalité nécessite un accès en écriture à l\'ensemble du dossier d\'installation. NE PAS UTILISER SUR UN SITE EN PRODUCTION !',
+);
